8a643733a39454d1c502dcf9fbe20c52e1f4ac10
aliyev_mehrojbek <aliyevmehrojbek0@gmail.com>
2024-01-17 14:59:52 +0500

    Added Account Menu

--- src/layout/nav/Nav.tsx
+++ src/layout/nav/Nav.tsx
@@ -1,12 +1,20 @@
 import "./Nav.scss"
+import  { useEffect, useRef, useState } from "react"
 import { Container } from "../../styled-component/Styled"
-import { Link, NavLink } from 'react-router-dom'
-import { useEffect, useState } from "react"
-import { IoIosSearch, IoMdClose  } from "react-icons/io";
 import  Data  from "../../db/data.json"
+import { Link, NavLink } from 'react-router-dom'
+import { IoIosSearch, IoMdClose  } from "react-icons/io";
 import { FaRegHeart, FaRegUser } from "react-icons/fa";
 import { PiShoppingCartSimpleBold } from "react-icons/pi"
 import { SiPuma } from "react-icons/si";
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+
 
 
 const Nav = () => {
@@ -14,6 +22,15 @@ const Nav = () => {
   const [clearBtn, setClearBtn] = useState<boolean>(false)
   const [inputValue, setInputValue] = useState("")
   console.log(inputValue);
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   
 
   const [dd, setDd] = useState<boolean>(false)
@@ -45,6 +62,19 @@ const Nav = () => {
       setClearBtn(false)
     }
   }, [inputValue])
+
+  const FormBox: any = useRef() 
+  const [formFocus, setFormFocus] = useState<boolean>(false)
+
+  useEffect(() =>{
+    if(formFocus){
+      FormBox.current.style = "box-shadow: 0 0 3px 3px gray"
+    } else{
+      FormBox.current.style = "box-shadow: 0 0 0px #fff"
+      
+      
+    }
+  }, [formFocus])
 
 
   return (
@@ -81,7 +111,75 @@ const Nav = () => {
               <div className="action-card">
                 <Link to={"/"} className="action-link"><FaRegHeart /></Link>
                 <Link to={"/"} className="action-link"><PiShoppingCartSimpleBold /></Link>
-                <Link to={"/"} className="action-link"><FaRegUser /></Link>
+                <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ mr: 1, cursor: "pointer" }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32, background: "transparent" }}><FaRegUser/></Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 2,
+            width: "300px",
+              '& .MuiAvatar-root': {
+              width: 32,
+              height: 2,
+              overflowY: "auto",
+              ml: -0.5,
+              mr: 1,
+            },
+            '&::before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+      
+        <MenuItem style={{marginTop: "10px"}}>  My Account</MenuItem>
+        <Divider />
+        <MenuItem className="menu__item-list">  Initiate Return</MenuItem>
+        <Divider />
+        <MenuItem className="menu__item-list">  Order Status</MenuItem>
+        <Divider />
+        <MenuItem className="menu__item-list">  Contact Us</MenuItem>
+        <Divider />
+        <MenuItem className="menu__item-list">  Wishlist</MenuItem>
+        <Divider />
+        <MenuItem className="menu__item-list">  Language</MenuItem>
+        <MenuItem className="login-item"><Link to={"/login"} className="profile__menu-login">LOGIN</Link></MenuItem>
+        <MenuItem className="login__item"><Link to={"/register"} className=" profile__menu-register">REGISTER HERE</Link></MenuItem>
+        
+      
+      </Menu>
               </div>
             </div>
           </div>
@@ -89,9 +187,9 @@ const Nav = () => {
         <div style={searchDronDown ? { transform: "scaleY(1)", transition: "0.3s" } : {transform: "scaleY(0)"}} className="search__content-wrapper">
                 <div className="dropdown__search-navigation">
               <Container>
-                <div className="form-wrapper">
-                <form className="search-form">
-                    <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} type="text" placeholder="SEARCH PUMA.COM" />
+                <div  className="form-wrapper">
+                <form  ref={FormBox} className="search-form">
+                    <input  onMouseLeave={() => setFormFocus(false)} onMouseEnter={() => setFormFocus(true) } value={inputValue} onChange={(e) => {setInputValue(e.target.value), setFormFocus(true)}} type="text" placeholder="SEARCH PUMA.COM" />
                     <button onClick={() => setInputValue("")} type="button" style={clearBtn ? {display: "block"} : {display: 'none'}} className="clear-btn">Clear</button>
                     <button type="button" className="dropdown__search-btn"><IoIosSearch/></button>
                   </form>
