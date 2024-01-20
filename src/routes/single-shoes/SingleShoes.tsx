ab2c757986d43b66083a4acc064d60f03386dd88
aliyev_mehrojbek <aliyevmehrojbek0@gmail.com>
2024-01-20 22:35:01 +0500

    Created store and setup

--- src/routes/single-shoes/SingleShoes.tsx
+++ src/routes/single-shoes/SingleShoes.tsx
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import { Container } from '../../styled-component/Styled';
 import ApiInstance from '../../api';
+import { ProductTypes } from '../../types';
+
+
 const SingleShoes = () => {
     const {id} = useParams()
-    console.log(id);
+    const [singleData, setSingleData] = useState([])
 
     useEffect(() => {
     async function loadData() {
       try {
-        const response = await ApiInstance(`/${id}`)
-        console.log(response);
+        const response = await ApiInstance(`/product/${id}`)
+        setSingleData(response.data.payload)
+        console.log(response.data.payload);  
         
       }
        catch (error) {
@@ -25,7 +29,11 @@ const SingleShoes = () => {
     <div>
       <Container>
         <div className="single-wrapper">
-          
+            {
+              singleData.map((data: ProductTypes) =>
+                  <img src={data.product_images[0]} alt="" />
+                )
+            }
         </div>
       </Container>
     </div>
