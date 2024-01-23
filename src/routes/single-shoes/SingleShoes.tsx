f053ff594ab7d4a03ffd204bc2dfeddaa9b9ff19
aliyev_mehrojbek <aliyevmehrojbek0@gmail.com>
2024-01-23 13:27:48 +0500

    Added Features page for cart page

--- src/routes/single-shoes/SingleShoes.tsx
+++ src/routes/single-shoes/SingleShoes.tsx
@@ -6,7 +6,7 @@ import { ProductTypes } from '../../types';
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import { addToCart } from '../../redux/slices/cart-slice';
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../redux/store/store';
 import "./SingleShoes.scss"
 import { TbTruckDelivery } from "react-icons/tb";
@@ -19,11 +19,13 @@ const SingleShoes = () => {
   const { id } = useParams()
   const [singleData, setSingleData] = useState([])
   const dispatch = useDispatch<AppDispatch>()
-  
+
+  const [addCartLoading, setAddCartLoading] = useState<boolean>(false)
   const handleAddToCart = (product: ProductTypes): void => {
-     console.log(product);
-     
-     dispatch(addToCart(product))
+    setAddCartLoading(true)
+    console.log(product);
+
+    dispatch(addToCart(product))
   }
 
   useEffect(() => {
@@ -32,14 +34,19 @@ const SingleShoes = () => {
         const response = await ApiInstance(`/product/${id}`)
         setSingleData(response.data.payload)
         console.log(response.data.payload);
-
       }
       catch (error) {
         console.log(error);
-
       }
     }
     loadData()
+  }, [])
+
+
+  useEffect(() => {
+     setTimeout(() => {
+      setAddCartLoading(false)
+    }, 3500)
   }, [])
 
   return (
@@ -62,13 +69,19 @@ const SingleShoes = () => {
                 <button>BEST SELLER</button>
                 <strong>${data.variants[0].variant_original_price}</strong>
                 <button className="explore-btn">EXPLORE THIS COLLECTION</button>
-                <p className='delivery-text'> <i><TbTruckDelivery/> </i>This item qualifies for free shipping!</p>
-                <p className='reload-text'><i><IoReloadOutline/></i> Free returns on all qualifying orders.</p>
-                <Divider/>
+                <p className='delivery-text'> <i><TbTruckDelivery /> </i>This item qualifies for free shipping!</p>
+                <p className='reload-text'><i><IoReloadOutline /></i> Free returns on all qualifying orders.</p>
+                <Divider />
                 <div className="card__btn-actions">
-                  <button onClick={() => handleAddToCart(data)} className='addcart-btn'>ADD TO CART</button>
+                  
+                  <button onClick={() => handleAddToCart(data)} style={addCartLoading ? {background: "#9a9c9e", cursor: "not-allowed"} : {background: "#000", cursor: "auto"}} className='addcart-btn'>
+                    {
+                      !addCartLoading ? "Add To Cart" : <div className="loader"></div>
+
+                    }
+                  </button>
                   <button className='addwishlist-btn'>
-                    <i><FaRegHeart/></i>
+                    <i><FaRegHeart /></i>
                     ADD TO WISHLIST
                   </button>
                 </div>
