3a6adc838613aac16d924e89e7a8542f0ffa5580
aliyev_mehrojbek <aliyevmehrojbek0@gmail.com>
2024-01-27 12:47:40 +0500

    Added Feature category

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,16 @@
+import FeatureCategory from "../../components/feature-category/FeatureCategory"
 import Hero from "../../components/hero/Hero"
 import MainHero from "../../components/main-hero/MainHero"
 import SoccerPage from "../../components/soccer-page/SoccerPage"
 import "./Home.scss"
 
 const Home = () => {
+
+  
   return (
     <div>
         <MainHero/>
+        <FeatureCategory/>
         <Hero/> 
         <SoccerPage/>
     </div>
