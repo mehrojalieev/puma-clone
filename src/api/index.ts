69bd14e378f26be1e955c8e125ad907dcc25b038
aliyev_mehrojbek <aliyevmehrojbek0@gmail.com>
2024-01-31 23:51:45 +0500

    Added User Table

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,8 @@ const ApiInstance: AxiosInstance = axios.create({
     baseURL: "http://localhost:2000/api",
     headers: {
         "Accept" : "application/json",
-        "Content-Type" : "application/json"
+        "Content-Type" : "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTM3NjY5MzQsInVzZXIiOnsiZW1haWwiOiJpai5pYnJva2hpbS5qYWxhbG92QGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsIl9pZCI6IjY1YWY1NzEwZjQ1OWJiZDRmZGY3NmI0YSJ9LCJpYXQiOjE3MDU5OTA5MzR9.hOaZs_9kzystRGMNO8Fe_zChLdmP-Zthx176Nk6XnPM"
     },
     timeout: 10000
 });
